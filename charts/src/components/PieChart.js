8e43db2770f468d834faebc93cb688b50ab304c9
onifernando1 <onifernando1@gmail.com>
2023-08-01 21:16:56 +0100

    pie chart js

---
+++ charts/src/components/PieChart.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import "../assets/styles/barchart.css";
+
+import { Chart as ChartJS, ArcElement, Legend, Tooltip, Title } from "chart.js";
+
+ChartJS.register(ArcElement, Legend, Tooltip, Title);
+
+function PieChart(props) {
+  const pieLabels = props.labels;
+  const dataset1 = props.dataset1;
+
+  const data = {
+    labels: pieLabels,
+    datasets: [
+      {
+        data: dataset1,
+        backgroundColor: ["aqua", "bloodorange", "purple"],
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: false,
+    },
+    scales: {
+      y: {
+        // min: 3,
+        // max: 6,
+      },
+    },
+  };
+
+  return (
+    <div className="pie-chart">
+      <Pie data={data} options={options}></Pie>
+    </div>
+  );
+}
+
+export default PieChart;
