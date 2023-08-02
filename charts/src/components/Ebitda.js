89752c877e4bd92673f07ec57f579a8f3dcc795c
onifernando1 <onifernando1@gmail.com>
2023-08-02 14:53:08 +0100

    refactor ebitda

--- charts/src/components/Ebitda.js
+++ charts/src/components/Ebitda.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import LineGraph from "./LineGraph";
 import "../assets/styles/ebitda.css";
 import { useEffect } from "react";
+import {
+  getPropertyArrayFromData,
+  getXYearsDataFromRequest,
+  getDateAndPropertyArrayFromData,
+} from "../functions/dataExtraction";
 
 function Ebitda(props) {
   const [dateArray, setDateArray] = useState("");
@@ -12,56 +17,11 @@ function Ebitda(props) {
     getDateAndEbitdaArraysFromOriginalRequest();
   }, [props.data]);
 
-  const getLastTenYearsFormRequestDataSorted = () => {
-    let tempLastTenYearsArray = [];
-    for (let i = 9; i >= 0; i--) {
-      tempLastTenYearsArray.push(requestData[i]);
-    }
-    return tempLastTenYearsArray;
-  };
-
-  const getDateAndEbitdaData = (data) => {
-    let tempArray = [];
-    for (let i = 0; i < data.length; i++) {
-      const tempDate = data[i].date;
-      const tempEbitda = data[i].ebitda;
-      const tempSortedData = [tempDate, tempEbitda];
-      tempArray.push(tempSortedData);
-    }
-    return tempArray;
-  };
-
-  const getDateAndEbitdaDataFromOriginalRequest = () => {
-    const lastTenYearsDataArray = getLastTenYearsFormRequestDataSorted();
-    const dateAndEbitdaArray = getDateAndEbitdaData(lastTenYearsDataArray);
-    // console.log(dateAndEbitdaArray);
-    return dateAndEbitdaArray;
-  };
-
-  const getDateArrayFromSortedData = (dataArray) => {
-    const tempDateArray = [];
-    for (let i = 0; i < dataArray.length; i++) {
-      tempDateArray.push(dataArray[i][0]);
-    }
-    // console.log(tempDateArray);
-    setDateArray(tempDateArray);
-    return tempDateArray;
-  };
-
-  const getEbitdaArrayFromSortedData = (dataArray) => {
-    const tempArray = [];
-    for (let i = 0; i < dataArray.length; i++) {
-      tempArray.push(dataArray[i][1]);
-    }
-    // console.log(tempArray);
-    setEbitdaArray(tempArray);
-    return tempArray;
-  };
-
   const getDateAndEbitdaArraysFromOriginalRequest = () => {
-    const joinedArray = getDateAndEbitdaDataFromOriginalRequest();
-    const dateArray = getDateArrayFromSortedData(joinedArray);
-    const ebitdaArray = getEbitdaArrayFromSortedData(joinedArray);
+    // const joinedArray = getDateAndEbitdaDataFrom
+    const lastTenYearsData = getXYearsDataFromRequest(requestData, 10);
+    setDateArray(getPropertyArrayFromData(lastTenYearsData, "date"));
+    setEbitdaArray(getPropertyArrayFromData(lastTenYearsData, "ebitda"));
   };
 
   return (
