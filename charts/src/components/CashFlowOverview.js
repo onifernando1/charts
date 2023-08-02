841442ea7b20b56c231ae537cd1b0ecc79a159c1
onifernando1 <onifernando1@gmail.com>
2023-08-02 16:12:23 +0100

    change to cash flow component on click

---
+++ charts/src/components/CashFlowOverview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CashFlowOverview(props) {
+  return (
+    <div className="cash-flow-overview-container">
+      <div>Cash Flow</div>
+    </div>
+  );
+}
+
+export default CashFlowOverview;
--- charts/src/components/OverviewManager.js
+++ charts/src/components/OverviewManager.js
@@ -3,25 +3,38 @@ import Overview from "./Overview";
 import { useState } from "react";
 import { amazonRequestData } from "../sampleData/requestData";
 import { MSFTRequestData } from "../sampleData/requestData";
+import CashFlowOverview from "./CashFlowOverview";
 
 function OverviewManager(props) {
+  const dataViewsArray = ["income", "cashFlow", "balance"];
   const [currentRequestData, setCurrentRequestData] =
     useState(amazonRequestData);
+  const [currentDataView, setCurrentDataView] = useState(dataViewsArray[0]);
   return (
     <>
       <div
         onClick={() => {
           setCurrentRequestData(amazonRequestData);
+          setCurrentDataView(dataViewsArray[0]);
         }}
       >
-        Amazon
+        Amazon Income
+      </div>
+      <div
+        onClick={() => {
+          setCurrentRequestData(amazonRequestData);
+          setCurrentDataView(dataViewsArray[1]);
+        }}
+      >
+        Amazon Cash Flow
       </div>
       <div
         onClick={() => {
           setCurrentRequestData(MSFTRequestData);
+          setCurrentDataView(dataViewsArray[0]);
         }}
       >
-        MSFT
+        MSFT Income
       </div>
       <div className="overview-title-container">
         <div>
@@ -34,8 +47,10 @@ function OverviewManager(props) {
           <div className="overview-watchlist">+ ADD TO WATCHLIST</div>
         </div>
       </div>
-      <Overview data={currentRequestData} /> /*Overview is actually Income Data
-      */
+      {currentDataView == "income" ? (
+        <Overview data={currentRequestData} />
+      ) : null}
+      {currentDataView == "cashFlow" ? <CashFlowOverview /> : null}
     </>
   );
 }
