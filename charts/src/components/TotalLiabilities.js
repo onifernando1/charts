865c7bd2d2fb50adfb6e4e01f85cf9b2338c8caa
onifernando1 <onifernando1@gmail.com>
2023-08-02 23:32:17 +0100

    total liabilities

---
+++ charts/src/components/TotalLiabilities.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import LineGraph from "./LineGraph";
+import { useEffect } from "react";
+import {
+  getPropertyArrayFromData,
+  getXYearsDataFromRequest,
+} from "../functions/dataExtraction";
+
+function TotalLiabilities(props) {
+  const [dateArray, setDateArray] = useState("");
+  const [totalLiabilities, setTotalLiabilities] = useState("");
+  const requestData = props.data;
+
+  useEffect(() => {
+    getDateAndCashEndDataFromRequest();
+  }, [props.data]);
+
+  const getDateAndCashEndDataFromRequest = () => {
+    const lastTenYearsData = getXYearsDataFromRequest(requestData, 10);
+    setDateArray(getPropertyArrayFromData(lastTenYearsData, "date"));
+    setTotalLiabilities(
+      getPropertyArrayFromData(lastTenYearsData, "totalLiabilities")
+    );
+  };
+
+  return (
+    <div className="default-container">
+      <div className="total-liabilities-title">Total Assets</div>
+      <LineGraph
+        x={dateArray}
+        dataset1={{ data: totalLiabilities, label: "$" }}
+      />
+    </div>
+  );
+}
+
+export default TotalLiabilities;
