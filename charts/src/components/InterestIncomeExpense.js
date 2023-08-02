1108c8332d808118ef57075155c2028a4199f3df
onifernando1 <onifernando1@gmail.com>
2023-08-02 14:59:16 +0100

    refactor interest income expenses

--- charts/src/components/InterestIncomeExpense.js
+++ charts/src/components/InterestIncomeExpense.js
@@ -2,6 +2,11 @@ import React from "react";
 import BarChart from "./BarChart";
 import { useState, useEffect } from "react";
 import "../assets/styles/incomeexpense.css";
+import {
+  getPropertyArrayFromData,
+  getXYearsDataFromRequest,
+  getDateAndPropertyArrayFromData,
+} from "../functions/dataExtraction";
 
 function InterestIncomeExpense(props) {
   const [dateArray, setDateArray] = useState("");
@@ -13,69 +18,15 @@ function InterestIncomeExpense(props) {
     getDateIncomeExpenseArraysFromOriginalRequest();
   }, [props.data]);
 
-  const getLastFiveYearsFormRequestDataSorted = () => {
-    let tempLastFiveYearsArray = [];
-    for (let i = 4; i >= 0; i--) {
-      tempLastFiveYearsArray.push(requestData[i]);
-    }
-    return tempLastFiveYearsArray;
-  };
-
-  const getDateInterestIncomeExpenseData = (data) => {
-    let tempArray = [];
-    for (let i = 0; i < data.length; i++) {
-      const tempDate = data[i].date;
-      const tempIncome = data[i].interestIncome;
-      const tempExpense = data[i].interestExpense;
-      const tempSortedData = [tempDate, tempIncome, tempExpense];
-      tempArray.push(tempSortedData);
-    }
-    return tempArray;
-  };
-
-  const getDateInterestIncomeExpenseDataFromOriginalRequest = () => {
-    const lastFiveYearsDataArray = getLastFiveYearsFormRequestDataSorted();
-    const dateIncomeExpense = getDateInterestIncomeExpenseData(
-      lastFiveYearsDataArray
-    );
-    return dateIncomeExpense;
-  };
-
-  const getDateArrayFromSortedData = (dataArray) => {
-    let tempDateArray = [];
-    for (let i = 0; i < dataArray.length; i++) {
-      tempDateArray.push(dataArray[i][0]);
-    }
-    // console.log(tempDateArray);
-    setDateArray(tempDateArray);
-    return tempDateArray;
-  };
-
-  const getIncomeArrayFromSortedData = (dataArray) => {
-    let tempArray = [];
-    for (let i = 0; i < dataArray.length; i++) {
-      tempArray.push(dataArray[i][1]);
-    }
-    // console.log(tempArray);
-    setIncomeArray(tempArray);
-    return tempArray;
-  };
-
-  const getExpenseArrayFromSortedData = (dataArray) => {
-    let tempArray = [];
-    for (let i = 0; i < dataArray.length; i++) {
-      tempArray.push(dataArray[i][2]);
-    }
-    // console.log(tempArray);
-    setExpenseArray(tempArray);
-    return tempArray;
-  };
-
   const getDateIncomeExpenseArraysFromOriginalRequest = () => {
-    const joinedArray = getDateInterestIncomeExpenseDataFromOriginalRequest();
-    getDateArrayFromSortedData(joinedArray);
-    getIncomeArrayFromSortedData(joinedArray);
-    getExpenseArrayFromSortedData(joinedArray);
+    const lastFiveYearsData = getXYearsDataFromRequest(requestData, 5);
+    setDateArray(getPropertyArrayFromData(lastFiveYearsData, "date"));
+    setIncomeArray(
+      getPropertyArrayFromData(lastFiveYearsData, "interestIncome")
+    );
+    setExpenseArray(
+      getPropertyArrayFromData(lastFiveYearsData, "interestExpense")
+    );
   };
 
   return (
