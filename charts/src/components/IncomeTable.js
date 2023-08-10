6008497da8e444a307c017bc2d378925ef4238cc
onifernando1 <onifernando1@gmail.com>
2023-08-10 18:59:04 +0100

    income graph data and styling

--- charts/src/components/IncomeTable.js
+++ charts/src/components/IncomeTable.js
@@ -13,6 +13,8 @@ import Table from "./Table";
 function IncomeTable(props) {
   const [dateArray, setDateArray] = useState("");
   const [revenueArray, setRevenueArray] = useState("");
+  const [costOfRevenueArray, setCostOfRevenueArray] = useState("");
+
   const [ebitdaArray, setEbitdaArray] = useState("");
   const [grossProfitArray, setGrossProfitArray] = useState("");
   const [netIncomeArray, setNetIncomeArray] = useState("");
@@ -75,7 +77,226 @@ function IncomeTable(props) {
       )
     );
 
-    const tempA = [incomeTable, revTable];
+    const costOfRevenueArray = createTableObject(
+      "Cost Of Revenue",
+      "costOfRevenue",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const grossProfit = createTableObject(
+      "Gross Profit",
+      "grossProfit",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const grossProfitRatio = createTableObject(
+      "Gross Profit Ratio",
+      "grossProfitRatio",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const researchAndDevelopmentExpenses = createTableObject(
+      "Research And Development Expenses",
+      "researchAndDevelopmentExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const generalAndAdministrativeExpenses = createTableObject(
+      "General And Admin Expenses",
+      "generalAndAdministrativeExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const sellingAndMarketingExpenses = createTableObject(
+      "Selling and Marketing Expenses",
+      "sellingAndMarketingExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const sellingGeneralAndAdministrativeExpenses = createTableObject(
+      "Selling General and Admin Expenses",
+      "sellingGeneralAndAdministrativeExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const otherExpenses = createTableObject(
+      "Other Expenses",
+      "otherExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const operatingExpenses = createTableObject(
+      "Operating Expenses",
+      "operatingExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const costAndExpenses = createTableObject(
+      "Cost And Expenses",
+      "costAndExpenses",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const interestIncome = createTableObject(
+      "Interest Income",
+      "interestIncome",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const interestExpense = createTableObject(
+      "Interest Expense",
+      "interestExpense",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const depreciationAndAmortization = createTableObject(
+      "Depreciation and Amortization",
+      "depreciationAndAmortization",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const ebitda = createTableObject(
+      "Ebitda",
+      "ebitda",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const ebitdaRatio = createTableObject(
+      "Ebitda Ratio",
+      "ebitdaratio",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const operatingIncome = createTableObject(
+      "Operating Income",
+      "operatingIncome",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const operatingIncomeRatio = createTableObject(
+      "Operating Income Ratio",
+      "operatingIncomeRatio",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const totalOtherIncomeExpensesNet = createTableObject(
+      "Total Other Income Expenses Net",
+      "totalOtherIncomeExpensesNet",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const incomeBeforeTax = createTableObject(
+      "Income Before Tax",
+      "incomeBeforeTax",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const incomeBeforeTaxRatio = createTableObject(
+      "Income Before Tax Ratio",
+      "incomeBeforeTaxRatio",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const incomeTaxExpense = createTableObject(
+      "Income Tax Expense",
+      "incomeTaxExpense",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const netIncome = createTableObject(
+      "Net Income",
+      "netIncome",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const netIncomeRatio = createTableObject(
+      "Net Income Ratio",
+      "netIncomeRatio",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const eps = createTableObject(
+      "EPS",
+      "eps",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const epsDiluted = createTableObject(
+      "EPS Diluted",
+      "epsdiluted",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const weightedAverageShsOut = createTableObject(
+      "Weighted Average SHS Out",
+      "weightedAverageShsOut",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const weightedAverageShsOutDil = createTableObject(
+      "Weighted Average SHS Out Dil",
+      "weightedAverageShsOutDil",
+      lastFiveYearsDataDescending,
+      yearArray
+    );
+
+    const tempA = [
+      incomeTable,
+      revTable,
+      costOfRevenueArray,
+      grossProfit,
+      grossProfitRatio,
+      researchAndDevelopmentExpenses,
+      generalAndAdministrativeExpenses,
+      sellingAndMarketingExpenses,
+      sellingGeneralAndAdministrativeExpenses,
+      otherExpenses,
+      operatingExpenses,
+      costAndExpenses,
+      interestIncome,
+      interestExpense,
+      depreciationAndAmortization,
+      ebitda,
+      ebitdaRatio,
+      operatingIncome,
+      operatingIncomeRatio,
+      totalOtherIncomeExpensesNet,
+      incomeBeforeTax,
+      incomeBeforeTaxRatio,
+      incomeTaxExpense,
+      netIncome,
+      netIncomeRatio,
+      eps,
+      epsDiluted,
+      weightedAverageShsOut,
+      weightedAverageShsOutDil,
+    ];
     setJoinedDataArray(tempA);
     console.log(tempA);
 
